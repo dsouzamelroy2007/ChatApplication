812b26241df574a62b13f81a02e78a4253afb203
dsouzamelroy2007 <dsouzamelroy2007@gmail.com>
2020-03-17 23:15:42 +0530

    stomp websocket application
    spring boot application using  stomp web socket

---
+++ src/main/java/com/websocketboot/app/SpringbootwebsocketchatapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.websocketboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwebsocketchatapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwebsocketchatapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/websocketboot/app/SpringbootwebsocketchatapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocketboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwebsocketchatapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
